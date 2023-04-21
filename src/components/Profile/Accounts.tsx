58e6bb3287596dd1af98f5f95d3ac9c2b5bceb5d
SmorzhaniukCinis <smorzhaniuk.cinis@gmail.com>
2023-04-21 19:25:40 +0300

    made bg layout for cards

--- src/components/Profile/Accounts.tsx
+++ src/components/Profile/Accounts.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Box from "@mui/material/Box";
 
 const Accounts:React.FC = () => {
+
+
+
   return (
-    <div>
-      Accounts
-    </div>
+    <Box sx={{display: 'grid', gridTemplateColumns: {md: '1fr 1fr', xs: '1fr'}, gridGap: 50}}>
+      <Box sx={{backgroundColor: '#000', width: '100%', aspectRatio: '10/6', borderRadius: 5, boxShadow: '2px 38px 15px -20px gray',}}></Box>
+      <Box sx={{backgroundColor: '#000', width: '100%', aspectRatio: '10/6', borderRadius: 5, boxShadow: '2px 38px 15px -20px gray',}}></Box>
+      <Box sx={{backgroundColor: '#000', width: '100%', aspectRatio: '10/6', borderRadius: 5, boxShadow: '2px 38px 15px -20px gray',}}></Box>
+      <Box sx={{backgroundColor: '#000', width: '100%', aspectRatio: '10/6', borderRadius: 5, boxShadow: '2px 38px 15px -20px gray',}}></Box>
+    </Box>
   );
 };
 
-export default Accounts;
+export default Accounts
